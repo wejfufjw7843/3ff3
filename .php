9b605c59def6015f24f5742ba10c1faacadb4df7
wejfufjw7843 <163233954+wejfufjw7843@users.noreply.github.com>
2024-03-18 12:14:08 +0600

    Add files via upload

---
+++ .php
@@ -0,0 +1,13 @@
+<?php
+// Функция для вывода имени и фамилии на экран
+function printName() {
+    let firstName = "Диас";
+    let lastName = "Бекболат";
+
+    console.log("Диас: " + firstName);
+    console.log("Бекболат: " + lastName);
+}
+
+// Вызов функции для вывода имени и фамилии на экран
+printName();
+?>
